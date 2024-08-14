4eb6c0ec6d5a03b70cb8471641ffb5d769434f7c
CyKrome007 <OwaizKhan1111@gmail.com>
2024-08-14 19:04:44 +0530

    Project Files Commit

---
+++ js/app.js
@@ -0,0 +1,88 @@
+// Currency API
+let base_api = "https://cdn.jsdelivr.net/npm/@fawazahmed0/currency-api@latest/v1/currencies/";
+
+const input = document.querySelector('input');
+const dropdowns = document.querySelectorAll(".dropdown select");
+const btn = document.querySelector("#convert-btn");
+const switchBtn = document.querySelector("#switch");
+const msg = document.querySelector(".msg");
+
+let fromCurrency = "INR";
+let toCurrency = "USD";
+let lightMode = true;
+
+switchBtn.addEventListener("click", () => {
+    if(lightMode){
+        for(let element of document.querySelectorAll('.light')){
+            element.classList.remove("light");
+            element.classList.add("dark");
+        }
+        for(let element of document.querySelectorAll('.light-nobg')){
+            element.classList.remove("light-nobg");
+            element.classList.add("dark-nobg");
+        }
+        for(let element of document.querySelectorAll('.light-border')){
+            element.classList.remove("light-border");
+            element.classList.add("dark-border");
+        }
+        switchBtn.classList.add('fa-sun')
+        switchBtn.classList.remove('fa-moon')
+        lightMode = false;
+    } else {
+        for(let element of document.querySelectorAll('.dark')){
+            element.classList.remove("dark");
+            element.classList.add("light");
+        }
+        for(let element of document.querySelectorAll('.dark-nobg')){
+            element.classList.remove("dark-nobg");
+            element.classList.add("light-nobg");
+        }
+        for(let element of document.querySelectorAll('.dark-border')){
+            element.classList.remove("dark-border");
+            element.classList.add("light-border");
+        }
+        switchBtn.classList.add('fa-moon')
+        switchBtn.classList.remove('fa-sun')
+        lightMode = true;
+    }
+
+})
+
+for(let select of dropdowns) {
+    select.addEventListener("change", (evt) => {
+        updateSelect(evt.target);
+    })
+}
+for(let select of dropdowns) {
+    for(let currCode in countryList){
+        let newOption = document.createElement("option");
+        newOption.innerText = currCode;
+        newOption.value = currCode;
+        if(currCode === "INR" && select.name === "from"){
+            newOption.selected = true;
+            fromCurrency = newOption.value;
+        } else if(currCode === "USD" && select.name === "to"){
+            newOption.selected = true;
+            toCurrency = newOption.value;
+        }
+        select.append(newOption);
+    }
+}
+
+btn.addEventListener("click",async (evt) => {
+    evt.preventDefault();
+    let response = await fetch(`${base_api}${fromCurrency.toLowerCase()}.json`);
+    let json = await response.json();
+    let oneValue = json[fromCurrency.toLowerCase()][toCurrency.toLowerCase()]; // Value
+    let userReq = input.value * oneValue;
+    msg.innerText = `${input.value} ${fromCurrency} = ${userReq} ${toCurrency}.`;
+})
+
+const updateSelect = (target) => {
+    if(target.name === 'to'){
+        toCurrency = target.value;
+    } else {
+        fromCurrency = target.value;
+    }
+    target.parentElement.parentElement.querySelector("img").src = `https://flagsapi.com/${countryList[target.value]}/flat/64.png`;
+}
