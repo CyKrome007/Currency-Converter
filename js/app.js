1803fc6c3fcca9a7fe046a51cff9d86e925f0c76
CyKrome007 <OwaizKhan1111@gmail.com>
2024-08-14 21:27:32 +0530

    Fixed Bug Covert Button not working and Standardized The Code

--- js/app.js
+++ js/app.js
@@ -1,17 +1,37 @@
-// Currency API
+/*
+* Author: Owaiz Mustafa Khan
+* Alias: CyKrome
+* Github: https://github.com/CyKrome007
+* Email: OwaizKhan1111@gmail.com
+* Date: 14-08-2024
+* Time: 21:27PM IST
+* */
+
+
+
+/*
+* Currency API
+* Note: This is not the final api endpoint.
+* The final API endpoint is further constructed in the code
+* */
 let base_api = "https://cdn.jsdelivr.net/npm/@fawazahmed0/currency-api@latest/v1/currencies/";
 
+/* HTML Web Page Elements Start */
 const input = document.querySelector('input');
 const dropdowns = document.querySelectorAll(".dropdown select");
 const btn = document.querySelector("#convert-btn");
 const switchBtn = document.querySelector("#switch");
 const msg = document.querySelector(".msg");
 const swap = document.querySelector("i");
+/* HTML Web Page Element End */
 
+/* Helper Variables Declaration and Initialization Start */
 let fromCurrency = "INR";
 let toCurrency = "USD";
 let lightMode = true;
+/* Helper Variables Declaration and Initialization End */
 
+// Dark Mode and Light mode toggle logic
 switchBtn.addEventListener("click", () => {
     if(lightMode){
         for(let element of document.querySelectorAll('.light')){
@@ -49,11 +69,14 @@ switchBtn.addEventListener("click", () => {
 
 })
 
+// Adding Event Listeners to Currency Selectors(from and to)
 for(let select of dropdowns) {
     select.addEventListener("change", (evt) => {
         updateSelect(evt.target);
     })
 }
+
+// Creating options for currency selectors
 for(let select of dropdowns) {
     for(let currCode in countryList){
         let newOption = document.createElement("option");
@@ -70,7 +93,19 @@ for(let select of dropdowns) {
     }
 }
 
+// This Function Updates the Selected Options(value and flag images)
+const updateSelect = (target) => {
+    if(target.name === 'to'){
+        toCurrency = target.value;
+    } else {
+        fromCurrency = target.value;
+    }
+    target.parentElement.parentElement.querySelector("img").src = `https://flagsapi.com/${countryList[target.value]}/flat/64.png`;
+}
+
+// Currency Swap Button
 swap.addEventListener("click", (e) => {
+    e.preventDefault();
     if(!swap.classList.contains("swap-anim"))
         swap.classList.add("swap-anim");
     setTimeout(() => {
@@ -80,26 +115,22 @@ swap.addEventListener("click", (e) => {
         dropdowns[1].value = newTo;
         updateSelect(dropdowns[0]);
         updateSelect(dropdowns[1]);
-        convert();
+        convert().then(() => {
+            // console.log('Switch Success');
+        });
     }, 700)
 })
 
+// Convert Button
 btn.addEventListener("click",async (evt) => {
     evt.preventDefault();
+    await convert();
 })
 
+// Conversion
 const convert = async () => {
     let json = await (await fetch(`${base_api}${fromCurrency.toLowerCase()}.json`)).json();
     let oneValue = json[fromCurrency.toLowerCase()][toCurrency.toLowerCase()]; // Value
     let userReq = input.value * oneValue;
     msg.innerText = `${input.value} ${fromCurrency} = ${userReq} ${toCurrency}.`;
-}
-
-const updateSelect = (target) => {
-    if(target.name === 'to'){
-        toCurrency = target.value;
-    } else {
-        fromCurrency = target.value;
-    }
-    target.parentElement.parentElement.querySelector("img").src = `https://flagsapi.com/${countryList[target.value]}/flat/64.png`;
 }
